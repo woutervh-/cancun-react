0333cf25c0a6f56e0e2a9db3fb2b0e19594b9dbf
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2016-01-12 16:01:27 +0100

    Simple rendering scripts

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-console.log("Success");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <h1>Welcome</h1>,
+    document.getElementById('react-main-mount')
+);
