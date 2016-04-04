157ff45a918a0d8747e37ec7d18f4097daa3a426
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2016-04-04 16:44:54 +0200

    Selectable tile size

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,11 @@ import React from 'react';
 import style from '../../public/stylesheets/style.css';
 
 export default class App extends React.Component {
+    constructor() {
+        super();
+        this.handleTileSizeChange = this.handleTileSizeChange.bind(this);
+    }
+
     static propTypes = {
         map: React.PropTypes.instanceOf(Map).isRequired
     };
@@ -12,14 +17,32 @@ export default class App extends React.Component {
         map: new Map()
     };
 
+    state = {
+        tileSize: 256
+    };
+
+    handleTileSizeChange(event) {
+        this.setState({tileSize: event.currentTarget.value});
+    }
+
     render() {
         return <div className={style['wrapper']}>
             <div className={style['content']}>
-                <MapView map={this.props.map} tileSize={512}/>
+                <MapView map={this.props.map} tileSize={this.state.tileSize}/>
             </div>
             <div className={style['menu']}>
                 <ul>
                     <li>Item 1</li>
+                    <li>
+                        <div>
+                            <input type="radio" id="tile-size-256" name="tile-size" checked={this.state.tileSize == 256} value={256} onChange={this.handleTileSizeChange}/>
+                            <label htmlFor="tile-size-256">256</label>
+                        </div>
+                        <div>
+                            <input type="radio" id="tile-size-512" name="tile-size" checked={this.state.tileSize == 512} value={512} onChange={this.handleTileSizeChange}/>
+                            <label htmlFor="tile-size-512">512</label>
+                        </div>
+                    </li>
                 </ul>
             </div>
         </div>;
