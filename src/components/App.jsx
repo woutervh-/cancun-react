1fb6acb7ee6455eb4b17a939dae37b7c13426a87
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2016-04-05 13:42:46 +0200

    Material-ui for UI components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -29,20 +29,8 @@ export default class App extends React.Component {
             <div className={style['content']}>
                 <MapView/>
             </div>
-            <div className={style['menu']}>
-                <ul>
-                    <li>Item 1</li>
-                    <li>
-                        <div>
-                            <input type="radio" id="tile-size-256" name="tile-size" checked={this.state.tileSize == 256} value={256} onChange={this.handleTileSizeChange}/>
-                            <label htmlFor="tile-size-256">256</label>
-                        </div>
-                        <div>
-                            <input type="radio" id="tile-size-512" name="tile-size" checked={this.state.tileSize == 512} value={512} onChange={this.handleTileSizeChange}/>
-                            <label htmlFor="tile-size-512">512</label>
-                        </div>
-                    </li>
-                </ul>
+            <div className={style['top-menu']}>
+                    This is the menu
             </div>
         </div>;
     }
