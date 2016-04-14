97638d18c08e1042a2bb96891ba3a37015882491
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2016-04-14 15:04:28 +0200

    Refactored map state to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import {AppBar, IconMenu, Layout, MenuDivider, MenuItem, Panel} from 'react-toolbox';
+import MapHelper from '../lib/MapHelper.js';
 import MapView from './MapView.jsx';
+import MathUtil from '../lib/MathUtil.js';
 import React from 'react';
 import SearchBar from './SearchBar.jsx';
 import style from './style.scss';
@@ -8,12 +10,96 @@ export default class App extends React.Component {
     constructor() {
         super();
         this.handleSearchSubmit = this.handleSearchSubmit.bind(this);
+        this.handleTouchStart = this.handleTouchStart.bind(this);
+        this.handleTouchMove = this.handleTouchMove.bind(this);
+        this.handleTouchEnd = this.handleTouchEnd.bind(this);
+        this.handleMouseDown = this.handleMouseDown.bind(this);
+        this.handleMouseMove = this.handleMouseMove.bind(this);
+        this.handleMouseUp = this.handleMouseUp.bind(this);
+        this.handleWheel = this.handleWheel.bind(this);
     }
 
     state = {
-        searchText: '',
-        searchDataSource: []
+        x: 0,
+        y: 0,
+        zoom: 0,
+        dragData: {
+            dragging: false
+        }
     };
+
+    handleTouchStart(event) {
+        event.button = 0;
+        event.clientX = event.touches[0].clientX;
+        event.clientY = event.touches[0].clientY;
+        this.handleMouseDown(event);
+    }
+
+    handleTouchMove(event) {
+        event.clientX = event.touches[0].clientX;
+        event.clientY = event.touches[0].clientY;
+        this.handleMouseMove(event);
+    }
+
+    handleTouchEnd(event) {
+        this.handleMouseUp(event);
+    }
+
+    handleMouseDown(event) {
+        if (event.button == 0) {
+            this.setState({
+                dragData: {
+                    dragging: true,
+                    startX: this.state.x,
+                    startY: this.state.y,
+                    startMouseX: event.clientX,
+                    startMouseY: event.clientY
+                }
+            });
+        }
+    }
+
+    handleMouseMove(event) {
+        if (this.state.dragData.dragging) {
+            let dx = event.clientX - this.state.dragData.startMouseX;
+            let dy = event.clientY - this.state.dragData.startMouseY;
+            this.setState({x: this.state.dragData.startX - dx, y: this.state.dragData.startY - dy});
+        }
+    }
+
+    handleMouseUp() {
+        this.setState({dragData: {dragging: false}});
+    }
+
+    handleWheel(event) {
+        let mapHelper = new MapHelper();
+        let container = this.refs.container;
+        let containerOffsetX = container.offsetLeft;
+        let containerOffsetY = container.offsetTop;
+        let parent = container.offsetParent;
+        while (parent != null) {
+            containerOffsetX += parent.offsetLeft;
+            containerOffsetY += parent.offsetTop;
+            parent = parent.offsetParent;
+        }
+        let alongX = MathUtil.norm(containerOffsetX, containerOffsetX + container.offsetWidth, event.clientX);
+        let alongY = MathUtil.norm(containerOffsetY, containerOffsetY + container.offsetHeight, event.clientY);
+
+        if (event.deltaY < 0 && this.state.zoom < mapHelper.maxZoom) {
+            this.setState({
+                zoom: this.state.zoom + 1,
+                x: this.state.x * 2 + container.offsetWidth * alongX,
+                y: this.state.y * 2 + container.offsetHeight * alongY
+            });
+        }
+        if (event.deltaY > 0 && this.state.zoom > mapHelper.minZoom) {
+            this.setState({
+                zoom: this.state.zoom - 1,
+                x: (this.state.x - container.offsetWidth * alongX) / 2,
+                y: (this.state.y - container.offsetHeight * alongY) / 2
+            });
+        }
+    }
 
     handleSearchSubmit(input) {
         if (!!input) {
@@ -24,7 +110,16 @@ export default class App extends React.Component {
 
     render() {
         return <span>
-            <MapView/>
+            <div onWheel={this.handleWheel}
+                 onTouchStart={this.handleTouchStart}
+                 onTouchMove={this.handleTouchMove}
+                 onTouchEnd={this.handleTouchEnd}
+                 onMouseDown={this.handleMouseDown}
+                 onMouseMove={this.handleMouseMove}
+                 onMouseUp={this.handleMouseUp}
+                 ref="container">
+                <MapView x={this.state.x} y={this.state.y} zoom={this.state.zoom}/>
+            </div>
             <AppBar className={style['top-bar']}>
                 <IconMenu icon='menu' position='top-left'>
                     <MenuItem value='download' caption='Download'/>
--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,6 +1,5 @@
 import Canvas from './canvas/Canvas.jsx';
 import MapHelper from '../lib/MapHelper.js';
-import MathUtil from '../lib/MathUtil.js';
 import Picture from './canvas/Picture.jsx';
 import React from 'react';
 import Rectangle from './canvas/Rectangle.jsx';
@@ -10,25 +9,18 @@ export default class MapView extends React.Component {
         super();
         this.componentDidMount = this.componentDidMount.bind(this);
         this.componentWillUnmount = this.componentWillUnmount.bind(this);
-        this.handleTouchStart = this.handleTouchStart.bind(this);
-        this.handleTouchMove = this.handleTouchMove.bind(this);
-        this.handleTouchEnd = this.handleTouchEnd.bind(this);
-        this.handleMouseDown = this.handleMouseDown.bind(this);
-        this.handleMouseMove = this.handleMouseMove.bind(this);
-        this.handleMouseUp = this.handleMouseUp.bind(this);
         this.handleResize = this.handleResize.bind(this);
-        this.handleWheel = this.handleWheel.bind(this);
     }
 
+    static propTypes = {
+        x: React.PropTypes.number.isRequired,
+        y: React.PropTypes.number.isRequired,
+        zoom: React.PropTypes.number.isRequired
+    };
+
     state = {
-        zoom: 0,
-        x: 0,
-        y: 0,
         width: window.innerWidth,
-        height: window.innerHeight,
-        dragData: {
-            dragging: false
-        }
+        height: window.innerHeight
     };
 
     componentDidMount() {
@@ -39,102 +31,29 @@ export default class MapView extends React.Component {
         window.removeEventListener('resize', this.handleResize);
     }
 
-    handleTouchStart(event) {
-        event.button = 0;
-        event.clientX = event.touches[0].clientX;
-        event.clientY = event.touches[0].clientY;
-        this.handleMouseDown(event);
-    }
-
-    handleTouchMove(event) {
-        event.clientX = event.touches[0].clientX;
-        event.clientY = event.touches[0].clientY;
-        this.handleMouseMove(event);
-    }
-
-    handleTouchEnd(event) {
-        this.handleMouseUp(event);
-    }
-
-    handleMouseDown(event) {
-        if (event.button == 0) {
-            this.setState({
-                dragData: {
-                    dragging: true,
-                    startX: this.state.x,
-                    startY: this.state.y,
-                    startMouseX: event.clientX,
-                    startMouseY: event.clientY
-                }
-            });
-        }
-    }
-
-    handleMouseMove(event) {
-        if (this.state.dragData.dragging) {
-            let dx = event.clientX - this.state.dragData.startMouseX;
-            let dy = event.clientY - this.state.dragData.startMouseY;
-            this.setState({x: this.state.dragData.startX - dx, y: this.state.dragData.startY - dy});
-        }
-    }
-
-    handleMouseUp() {
-        this.setState({dragData: {dragging: false}});
-    }
-
     handleResize() {
         this.setState({width: window.innerWidth, height: window.innerHeight});
-    }
-
-    handleWheel(event) {
-        let mapHelper = new MapHelper();
-        let container = this.refs.container;
-        let containerOffsetX = container.offsetLeft;
-        let containerOffsetY = container.offsetTop;
-        let parent = container.offsetParent;
-        while (parent != null) {
-            containerOffsetX += parent.offsetLeft;
-            containerOffsetY += parent.offsetTop;
-            parent = parent.offsetParent;
-        }
-        let alongX = MathUtil.norm(containerOffsetX, containerOffsetX + container.offsetWidth, event.clientX);
-        let alongY = MathUtil.norm(containerOffsetY, containerOffsetY + container.offsetHeight, event.clientY);
-
-        if (event.deltaY < 0 && this.state.zoom < mapHelper.maxZoom) {
-            this.setState({
-                zoom: this.state.zoom + 1,
-                x: this.state.x * 2 + container.offsetWidth * alongX,
-                y: this.state.y * 2 + container.offsetHeight * alongY
-            });
-        }
-        if (event.deltaY > 0 && this.state.zoom > mapHelper.minZoom) {
-            this.setState({
-                zoom: this.state.zoom - 1,
-                x: (this.state.x - container.offsetWidth * alongX) / 2,
-                y: (this.state.y - container.offsetHeight * alongY) / 2
-            });
-        }
     }
 
     render() {
         let mapHelper = new MapHelper();
         let startTile = {
-            x: Math.floor(this.state.x / mapHelper.tileWidth),
-            y: Math.floor(this.state.y / mapHelper.tileHeight)
+            x: Math.floor(this.props.x / mapHelper.tileWidth),
+            y: Math.floor(this.props.y / mapHelper.tileHeight)
         };
         let endTile = {
-            x: Math.ceil((this.state.x + this.state.width) / mapHelper.tileWidth),
-            y: Math.ceil((this.state.y + this.state.height) / mapHelper.tileHeight)
+            x: Math.ceil((this.props.x + this.state.width) / mapHelper.tileWidth),
+            y: Math.ceil((this.props.y + this.state.height) / mapHelper.tileHeight)
         };
         let offset = {
-            x: startTile.x * mapHelper.tileWidth - this.state.x,
-            y: startTile.y * mapHelper.tileHeight - this.state.y
+            x: startTile.x * mapHelper.tileWidth - this.props.x,
+            y: startTile.y * mapHelper.tileHeight - this.props.y
         };
         let tiles = [];
         for (let i = 0; i < endTile.x - startTile.x; i++) {
             for (let j = 0; j < endTile.y - startTile.y; j++) {
                 tiles.push({
-                    url: mapHelper.getTileUrl(this.state.zoom, startTile.x + i, startTile.y + j),
+                    url: mapHelper.getTileUrl(this.props.zoom, startTile.x + i, startTile.y + j),
                     left: mapHelper.tileWidth * i + offset.x,
                     top: mapHelper.tileHeight * j + offset.y,
                     width: mapHelper.tileWidth,
@@ -152,18 +71,9 @@ export default class MapView extends React.Component {
             return adr - bdr;
         });
 
-        return <div onWheel={this.handleWheel}
-                    onTouchStart={this.handleTouchStart}
-                    onTouchMove={this.handleTouchMove}
-                    onTouchEnd={this.handleTouchEnd}
-                    onMouseDown={this.handleMouseDown}
-                    onMouseMove={this.handleMouseMove}
-                    onMouseUp={this.handleMouseUp}
-                    ref="container">
-            <Canvas ref="canvas" width={this.state.width} height={this.state.height} tabIndex={0}>
-                {tiles.map((tile, index) => <Picture key={index} source={tile.url} left={tile.left} top={tile.top} width={tile.width} height={tile.height}/>)}
-                <Rectangle width={this.state.width} height={this.state.height} strokeStyle="rgba(255, 0, 0, 1)" fillStyle="rgba(0, 0, 0, 0)"/>
-            </Canvas>
-        </div>;
+        return <Canvas ref="canvas" width={this.state.width} height={this.state.height} tabIndex={0}>
+            {tiles.map((tile, index) => <Picture key={index} source={tile.url} left={tile.left} top={tile.top} width={tile.width} height={tile.height}/>)}
+            <Rectangle width={this.state.width} height={this.state.height} strokeStyle="rgba(255, 0, 0, 1)" fillStyle="rgba(0, 0, 0, 0)"/>
+        </Canvas>;
     }
 };
