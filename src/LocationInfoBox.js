3ac29adb504b59d6f70e53767581b2dea5a81cab
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2016-05-05 10:23:43 +0200

    Opacity with Context Menu working

--- src/LocationInfoBox.js
+++ src/LocationInfoBox.js
@@ -33,21 +33,19 @@ export default class LocationInfoBox extends React.Component {
     }
 
     render() {
-        return <div style={{display: this.props.active ? 'block' : 'none'}}> /* TODO: get opacity transition to work with context menu */
-            <div className={classNames(style['location-box'], {[style['active']]: this.props.active})}>
-                <div className={style['location-box-info']}>
-                    <header>
-                        {this.props.locationInformation.name}
-                    </header>
-                    <p>
-                        {this.props.locationInformation.location.latitude},
-                        {this.props.locationInformation.location.longitude}
-                    </p>
-                </div>
-                <div className={style['location-box-actions']}>
-                    <Button onClick={this.props.onClearClick} raised={false}><FontIcon value="clear" className={style['button-icon']}/> Clear Result</Button>
-                    <Button onClick={this.props.onSendLocation} raised={false}><SendLocation viewBox="0 0 20 20"/> Send Location</Button>
-                </div>
+        return <div className={classNames(style['location-box'], {[style['active']]: this.props.active})}>
+            <div className={style['location-box-info']}>
+                <header>
+                    {this.props.locationInformation.name}
+                </header>
+                <p>
+                    {this.props.locationInformation.location.latitude},
+                    {this.props.locationInformation.location.longitude}
+                </p>
+            </div>
+            <div className={style['location-box-actions']}>
+                <Button onClick={this.props.onClearClick} raised={false}><FontIcon value="clear" className={style['button-icon']}/> Clear Result</Button>
+                <Button onClick={this.props.onSendLocation} raised={false}><SendLocation viewBox="0 0 20 20"/> Send Location</Button>
             </div>
         </div>;
     }
