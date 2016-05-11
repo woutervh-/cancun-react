2744f8c3570eceb00b4a815374076810d383cb37
Wouter van Heeswijk <woutervh@gmail.com>
2016-05-11 15:46:20 +0200

    Mobile friendly toolbar

--- src/TopBar.js
+++ src/TopBar.js
@@ -10,13 +10,18 @@ import EyeInactive from '../public/images/eye-inactive';
 import MapInactive from '../public/images/map-inactive';
 import LocalStorageComponent from './LocalStorageComponent';
 import {MapSelect, ToolbarItem} from './Toolbar';
+import EventUtil from './EventUtil';
 
 export default class TopBar extends LocalStorageComponent {
     constructor() {
         super();
         this.componentDidMount = this.componentDidMount.bind(this);
+        this.componentDidUpdate = this.componentDidUpdate.bind(this);
+        this.componentWillUnmount = this.componentWillUnmount.bind(this);
         this.shouldComponentUpdate = this.shouldComponentUpdate.bind(this);
         this.handlePinClick = this.handlePinClick.bind(this);
+        this.handleMenuClick = this.handleMenuClick.bind(this);
+        this.handleDocumentClick = this.handleDocumentClick.bind(this);
     }
 
     static propTypes = {
@@ -36,12 +41,30 @@ export default class TopBar extends LocalStorageComponent {
     };
 
     state = {
-        pinned: false
+        pinned: false,
+        toolbarActive: false
     };
 
     componentDidMount() {
         this.setPersistenceKey('top-bar');
         this.restoreState();
+        if (this.state.toolbarActive) {
+            document.addEventListener('mousedown', this.handleDocumentClick);
+        }
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.toolbarActive && !prevState.toolbarActive) {
+            document.addEventListener('mousedown', this.handleDocumentClick);
+        } else if (!this.state.toolbarActive && prevState.toolbarActive) {
+            document.removeEventListener('mousedown', this.handleDocumentClick);
+        }
+    }
+
+    componentWillUnmount() {
+        if (this.state.toolbarActive) {
+            document.removeEventListener('mousedown', this.handleDocumentClick);
+        }
     }
 
     shouldComponentUpdate(nextProps, nextState) {
@@ -50,10 +73,21 @@ export default class TopBar extends LocalStorageComponent {
             || nextProps.onSearchClear != this.props.onSearchClear
             || nextProps.onMapSelect != this.props.onMapSelect
             || nextState.pinned != this.state.pinned
+            || nextState.toolbarActive != this.state.toolbarActive;
     }
 
     handlePinClick() {
         this.setState({pinned: !this.state.pinned});
+    }
+
+    handleMenuClick() {
+        this.setState({toolbarActive: !this.state.toolbarActive});
+    }
+
+    handleDocumentClick(event) {
+        if (!EventUtil.targetIsDescendant(event, this.refs.toolbarContainer)) {
+            this.setState({toolbarActive: false});
+        }
     }
 
     render() {
@@ -65,9 +99,14 @@ export default class TopBar extends LocalStorageComponent {
                         : <EyeInactive viewBox="0 0 30 30"/>}
                 </IconButton>
                 <SearchBar onSubmit={this.props.onSearchSubmit} onClear={this.props.onSearchClear}/>
-                <ToolbarItem icon={<MapInactive viewBox="0 0 30 30"/>} label="Map">
-                    <MapSelect selected={this.props.mapStyle} options={MapHelper.styles} onMapSelect={this.props.onMapSelect}/>
-                </ToolbarItem>
+                <div ref="toolbarContainer">
+                    <IconButton icon="menu" onClick={this.handleMenuClick} className={style['toggle-side-bar-button']}/>
+                    <div className={classNames(style['toolbar-container'], {[style['active']]: this.state.toolbarActive})}>
+                        <ToolbarItem icon={<MapInactive viewBox="0 0 30 30"/>} label="Map">
+                            <MapSelect selected={this.props.mapStyle} options={MapHelper.styles} onMapSelect={this.props.onMapSelect}/>
+                        </ToolbarItem>
+                    </div>
+                </div>
             </div>
         </div>;
     }
