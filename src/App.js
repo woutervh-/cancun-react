851dc5564d754a3c53a658e3b18f7219cbd4449c
Wouter van Heeswijk <wouter.vanheeswijk@tomtom.com>
2016-06-02 16:06:17 +0200

    Removed unused code

--- src/App.js
+++ src/App.js
@@ -251,38 +251,5 @@ export default class App extends LocalStorageComponent {
                 {this.renderMarker()}
             </Map>
         </div>;
-
-        //return <span>
-        //    <TopBar onSearchSubmit={this.handleSearchSubmit}
-        //            onSearchClear={this.handleSearchClear}
-        //            onMapSelect={this.handleMapSelect}
-        //            mapStyle={this.state.mapStyle}
-        //            onTrafficChange={this.handleTrafficChange}
-        //            onTrafficToggle={this.handleTrafficToggle}
-        //            traffic={this.state.traffic}/>
-        //    <MapView view={this.state.view} onViewChange={this.handleViewChange} onLongViewChange={this.handleLongViewChange} onLocationSelect={this.handleLocationSelect} onTap={this.handleMapTap}>
-        //        <MapTilesLayer tileProvider={MapHelper} style={this.state.mapStyle} displayCachedTiles={true}/>
-        //        <MapTilesLayer tileProvider={FlowHelper} style={this.state.traffic.flowStyle} active={this.state.traffic.show && this.state.traffic.showFlow}/>
-        //        <MapTilesLayer tileProvider={TrafficHelper} style="s3" active={this.state.traffic.show && this.state.traffic.showTubes}/>
-        //        {this.state.traffic.show && this.state.traffic.showIcons
-        //            ? this.state.traffic.trafficIcons.map((poi, index) => {
-        //            let {p: {x: longitude, y: latitude}} = poi;
-        //            const IconType = IncidentsHelper.lookupIconType(poi);
-        //            // <IconType viewBox="0 0 20 20"/>
-        //            return <MapLayer key={index} latitude={latitude} longitude={longitude} render="html">
-        //                <img src="images/place-holder.svg" width="20" height="20"/>
-        //            </MapLayer>;
-        //        })
-        //            : null}
-        //        <MapLayer {...this.state.locationMarkerInformation.location} render="html" active={this.state.locationMarker.show}>
-        //            <Marker onTap={this.handleLocationMarkerTap} style={{width: '2rem', height: '3rem'}}>
-        //                <SearchMarker viewBox="0 0 20 30"/>
-        //            </Marker>
-        //        </MapLayer>
-        //        <MapLayer {...this.state.locationBoxInformation.location} render="html" active={this.state.locationBox.show}>
-        //            <LocationInfoBox onClearClick={this.handleSearchClear} locationInformation={this.state.locationBoxInformation}/>
-        //        </MapLayer>
-        //    </MapView>
-        //</span>;
     }
 };
